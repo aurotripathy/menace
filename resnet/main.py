d244fcb8c5b5f9d13e93ef4ed5d42fcf14863864
Auro Tripathy <ipserv@yahoo.com>
2018-12-22 10:51:15 -0800

    resnet/

---
+++ resnet/main.py
@@ -0,0 +1,128 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from resnet_18 import BasicBlock, Bottleneck, ResNet
+import torch.utils.model_zoo as model_zoo
+from pudb import set_trace
+    
+def train(args, model, device, train_loader, optimizer, criterion, epoch):
+
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader, 0):
+        inputs, labels = inputs.to(device), labels.to(device)        
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % args.log_interval == 0:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / args.log_interval))
+            running_loss = 0.0
+
+def test(args, model, device, test_loader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for (inputs, labels) in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the test images: %d %%' % (
+        100 * correct / total))
+
+            
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Example')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=300, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.001)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')    
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+
+    # Load and normalizing the CIFAR10 training and test datasets using torchvision    
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.1307,), (0.3081,))])
+    
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../cifar10_data', train=True, download=True,
+                         transform=transform),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../cifar10_data', train=False,
+                         transform=transform),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    # Define a Convolutional Neural Network
+    model = ResNet(Bottleneck, [3, 4, 6, 3], 10, True).to(device)
+
+    # Define a loss function
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    
+    # Train and test the network on the training data
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, criterion, epoch)
+        test(args, model, device, test_loader)
+
+    # Test the network on the test data
+    # ??
+
+    if (args.save_model):
+        torch.save(model.state_dict(),"mnist_cnn.pt")
+        
+if __name__ == '__main__':
+    main()
