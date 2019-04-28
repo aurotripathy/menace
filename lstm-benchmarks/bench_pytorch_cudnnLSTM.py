d4696b6ef9c161b0c60cb2f7478a2eb12fb5366d
Auro Tripathy <ipserv@yahoo.com>
2019-04-28 16:39:13 -0700

    redo

--- lstm-benchmarks/bench_pytorch_cudnnLSTM.py
+++ lstm-benchmarks/bench_pytorch_cudnnLSTM.py
@@ -1,105 +1,95 @@
 """
 Attributions:
-code from https://github.com/stefbraun/rnn_benchmarks (minor changes)
+code loosely based on https://github.com/stefbraun/rnn_benchmarks
 Arxiv paper https://arxiv.org/abs/1806.01818
-Essentially, a single-layer LSTM with 
+Description:
+Benchmarking a single-layer LSTM with:
 - 320 hidden units
 - 100 time steps
 - batch size 64
-- 1D input feature size, 123
+- 1D input feature size, 125
 - output 10 classes
+
+LSTMCell takes ONE input x_t at time t.
+    You need to loop in order to do one pass of backprop through time.
+
+LSTM takes a SEQUENCE of inputs x_1,x_2,…,x_T.
+    NO need to write a loop to do one pass of backprop through time.
 """
 
-import os
 import time as timer
-
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
+from support import get_batch, set_hyperparams, print_results
 
-from support import toy_batch, default_hyperparams, print_results, check_results
-
-def get_paramter_count():
-    params = 0
-    for param in list(net.parameters()):
-        sizes = 1
-        for el in param.size():
-            sizes = sizes * el
-            params += sizes
-    return params
-    
-
-# Get data
-bX, _, bY, classes = toy_batch()
-batch_size, seq_len, inp_dims = bX.shape
-hidden_units_size, learning_rate, batches = default_hyperparams()
-print("Hidden units:{}, Learning Rate:{}, Batches:{}".format(hidden_units_size,
-                                                             learning_rate, batches))
-
-# PyTorch compatibility: time first, batch second
-bX = np.transpose(bX, (1, 0, 2))
-
-# Create Network
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.lstm = nn.LSTM(input_size=inp_dims, hidden_size=hidden_units_size, num_layers=1,
+        self.lstm = nn.LSTM(input_size=in_dim, hidden_size=hidden_units_size, num_layers=1,
                             bias=True, bidirectional=False)
-        self.fc = nn.Linear(hidden_units_size, classes, bias=False)
+        self.fully_connect = nn.Linear(hidden_units_size, classes, bias=False)
 
     def forward(self, x):
         h1, state = self.lstm(x)
         h2 = h1[-1, :, :]
-        h3 = self.fc(h2)
+        h3 = self.fully_connect(h2)
         return h3
 
 
-net = Net()
-net.cuda()
+def validate_lstm_in_out():
+    """ Detect any dimension mismatch issues"""
+    assert net.fully_connect.in_features == hidden_units_size
+    assert (net.fully_connect.weight.cpu().data.numpy().shape == (
+        classes, hidden_units_size))  # final projection output size (classes, hidden_units_size)
+    t_b_X = Variable(torch.from_numpy(time_first_batch_of_X).cuda())
+    torch.cuda.synchronize()
+    output = net(t_b_X)
+    output_numpy = output.data.cpu().numpy()
+    assert output_numpy.shape == (batch_size, classes)
 
-params = get_paramter_count()
 
-# Create optimizer
-optimizer = optim.Adam(net.parameters(), lr=learning_rate)
-criterion = nn.CrossEntropyLoss()  # loss definition
+def train_lstm():
+    """ train for a nb_batches """
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()  # loss definition
 
-# Check for correct sizes
-assert (net.fc.in_features == hidden_units_size)  # final projection input size (hidden_unit_size)
-assert (net.fc.weight.cpu().data.numpy().shape == (
-    classes, hidden_units_size))  # final projection output size (classes, hidden_units_size)
-bXt = Variable(torch.from_numpy(bX).cuda())
-torch.cuda.synchronize()
-output = net(bXt)
-output_numpy = output.data.cpu().numpy()
-assert (output_numpy.shape == (batch_size, classes))
+    batch_time = []
+    batch_loss = []
+    print("Starting the training benchmark for {} batches".format(nb_batches))
+    train_start = timer.clock()
+    for _ in range(nb_batches):
+        torch.cuda.synchronize() # synchronize function call for precise time measurement
+        batch_start = timer.clock()
 
-# Start training
-batch_time = []
-batch_loss = []
-print("Starting the training benchmark with {} batches".format(batches))
-train_start = timer.clock()
-for _ in range(batches):
-    torch.cuda.synchronize() # synchronize function call for precise time measurement
-    batch_start = timer.clock()
+        t_b_X = Variable(torch.from_numpy(time_first_batch_of_X).cuda())
+        b_y = Variable(torch.from_numpy(batch_of_y).cuda())
 
-    bXt = Variable(torch.from_numpy(bX).cuda())
-    bYt = Variable(torch.from_numpy(bY).cuda())
+        optimizer.zero_grad()
+        output = net(t_b_X)
+        loss = criterion(output, b_y.long())
+        loss.backward()
+        optimizer.step()
 
-    optimizer.zero_grad()
-    output = net(bXt)
-    loss = criterion(output, bYt.long())
-    loss.backward()
-    optimizer.step()
+        torch.cuda.synchronize() # synchronize function call for precise time measurement
+        batch_time.append(timer.clock() - batch_start)
+        batch_loss.append(float(loss.data.cpu().numpy()))
+    train_end = timer.clock()
+    print_results(batch_loss, batch_time, train_start, train_end)
 
-    torch.cuda.synchronize() # synchronize function call for precise time measurement
-    batch_end = timer.clock()
-    batch_time.append(batch_end - batch_start)
-    batch_loss.append(float(loss.data.cpu().numpy()))
-train_end = timer.clock() # end of training
+if __name__ == '__main__':
 
-# Write results
-print_results(batch_time)
-check_results(batch_loss, batch_time, train_start, train_end)
+    hidden_units_size, learning_rate, seq_len, batch_size, nb_batches = set_hyperparams()
+    classes = 10
+    in_dim = 125
+    time_first_batch_of_X, batch_of_y = get_batch(shape=(seq_len, batch_size, in_dim), classes=classes)
+    print("Hidden units:{}, Learning Rate:{}, LSTM time steps:{} Batch size:{}, Batches:{}".format(hidden_units_size,
+                                                                                                   learning_rate, seq_len,
+                                                                                                   batch_size, nb_batches))
 
+    net = Net()
+    net.cuda()
+    validate_lstm_in_out()
+    print('Trainable params', sum(p.numel() for p in net.parameters() if p.requires_grad))
+    train_lstm()
