4db50b0febd353be5000d72f95268dbdfd031818
Auro Tripathy <ipserv@yahoo.com>
2019-04-28 11:46:05 -0700

    simplified version

--- lstm-benchmarks/support.py
+++ lstm-benchmarks/support.py
@@ -1,19 +1,20 @@
 import sys
 from collections import OrderedDict
 
-import matplotlib.pyplot as plt
 import numpy as np
 import os.path
 import pandas as pd
 
-def default_hyperparams():
-    rnn_size = 320
+def set_hyperparams():
+    lstm_size = 320
     learning_rate = 1e-3
+    seq_len = 100
+    batch_size = 64
     batches = 500
-    return rnn_size, learning_rate, batches
+    return lstm_size, learning_rate, seq_len, batch_size, batches
 
 
-def toy_batch(seed=11, shape=(64, 100, 123), classes=10):
+def get_batch(seed=11, shape=(64, 100, 123), classes=10):
     batch_size, max_len, features = shape
     np.random.seed(seed)
 
@@ -24,27 +25,8 @@ def toy_batch(seed=11, shape=(64, 100, 123), classes=10):
     # Targets
     bY = np.int32(np.random.randint(low=0, high=classes - 1, size=batch_size))
 
-    return bX, b_lenX, bY, classes
+    return bX, bY
 
-
-def toy_batch_ctc(seed=11, shape=(32, 1000, 123), classes=59):
-    batch_size, max_len, features = shape
-    np.random.seed(seed)
-
-    # Samples
-    bX = np.float32(np.random.uniform(-1, 1, (shape)))
-    b_lenX = np.int32(np.linspace(max_len / 2, max_len, batch_size))
-    # print(b_lenX)
-    maskX = np.zeros((batch_size, max_len), dtype='float32')
-    for i, len_sample in enumerate(b_lenX):
-        maskX[i, :len_sample] = np.ones((1, len_sample))
-
-    # Targets
-    bY = np.int32(np.random.randint(low=1, high=classes - 1,
-	                            size=batch_size * 100))  # remember warp-ctc: 0 is the blank label, tensorflow-ctc: -1 is the blank label
-    b_lenY = np.int32(np.ones(batch_size) * 100)  # labels per sample comes from WSJ-si84
-    
-    return bX, b_lenX, maskX, bY, b_lenY, classes
 
 
 def check_results(batch_loss_list, batch_time_list, train_start, train_end):
@@ -136,38 +118,3 @@ def print_results(run_time):
                 np.mean(run_time), np.std(run_time),
                 np.median(run_time), np.percentile(run_time, 99), np.min(run_time), np.max(run_time)))
 
-def plot_results(time):
-    fig, ax = plt.subplots()
-    ax.scatter(range(len(time)), time)
-    ax.grid()
-    ax.set_xlabel('Batch #')
-    ax.set_ylabel('Time per Batch [sec]')
-    return fig, ax
-
-
-# Helper functions for label conversion from warp-ctc to tf-ctc format:-(
-def target_converter(bY, b_lenY):
-    b_lenY_cs = np.cumsum(b_lenY)[:-1]
-    bY_conv = np.split(bY, b_lenY_cs)
-    return bY_conv
-
-
-def sparse_tuple_from(sequences, dtype=np.int32):
-    """Create a sparse representention of x.
-    Args:
-    sequences: a list of lists of type dtype where each element is a sequence
-    Returns:
-    A tuple with (indices, values, shape)
-    """
-    indices = []
-    values = []
-
-    for n, seq in enumerate(sequences):
-        indices.extend(zip([n] * len(seq), range(len(seq))))
-        values.extend(seq)
-
-    indices = np.asarray(indices, dtype=np.int64)
-    values = np.asarray(values, dtype=dtype)
-    shape = np.asarray([len(sequences), np.asarray(indices).max(0)[1] + 1], dtype=np.int64)
-
-    return indices, values, shape
