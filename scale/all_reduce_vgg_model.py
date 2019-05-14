5bbf4a765127542e418dd717c14412b15edf901c
Auro Tripathy <ipserv@yahoo.com>
2019-05-14 23:28:48 +0000

    repeat a few time, discard a few

--- scale/all_reduce_vgg_model.py
+++ scale/all_reduce_vgg_model.py
@@ -14,30 +14,35 @@ import time as timer
 import sys
 
 size = 138000000
-rand_val = 5.0
+const_val = 5.0
 
 nGPUs = torch.cuda.device_count()
 
 
-def all_reduce_vgg_model_size():
-    tensors = [torch.FloatTensor(size).fill_(rand_val) for i in range(nGPUs)]  # dim size, value random
-    expected = torch.FloatTensor(size).zero_()
-    for tensor in tensors:
-        expected.add_(tensor)  # add in-place
+def time_all_reduce_vgg_model_size(repeat=12, discard=2):
+    print('repeat', repeat, 'discard', discard)
+    times_per_iteration = []
+    for _ in range(repeat):
+        tensors = [torch.FloatTensor(size).fill_(const_val) for i in range(nGPUs)]  # dim size, value random
+        expected = torch.FloatTensor(size).zero_()
+        for tensor in tensors:
+            expected.add_(tensor)  # add in-place on CPU
 
-    tensors = [tensors[i].cuda(i) for i in range(nGPUs)]  # move ith tensor into ith GPU
-    
-    start_time = timer.time()
-    nccl.all_reduce(tensors)
-    time_taken = timer.time() - start_time
-    for tensor in tensors:
-        assert torch.all(torch.eq(tensor.cpu(), expected))  # move to CPU and compare
-    return time_taken
+        tensors = [tensors[i].cuda(i) for i in range(nGPUs)]  # move ith tensor into ith GPU
 
-_ = all_reduce_vgg_model()  # throw this one away as "warm up"
-reduction_time = all_reduce_vgg_model_size()
+        start_time = timer.time()
+        nccl.all_reduce(tensors)
+        times_per_iteration.append(timer.time() - start_time)
+        for tensor in tensors:
+            assert torch.all(torch.eq(tensor.cpu(), expected))  # move to CPU and compare
+    times_per_iteration = times_per_iteration[discard:]  # discard first few
+    print(len(times_per_iteration), times_per_iteration)
+    avg_time_taken = sum(times_per_iteration)/(repeat - discard)
+    return avg_time_taken
+
+reduction_time = time_all_reduce_vgg_model_size(22, 2)
 
 print('Python VERSION:', sys.version)
 print('pyTorch VERSION:', torch.__version__)
 print ('Available GPUs ', nGPUs)
-print("Time taken:{:.3f} seconds".format(reduction_time))
+print("Time taken:{:.9f} seconds".format(reduction_time))
