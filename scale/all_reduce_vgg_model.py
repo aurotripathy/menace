e81c12eb5a0ce40deaa972db1b80cdc8ad381618
Auro Tripathy <ipserv@yahoo.com>
2019-05-15 18:42:26 +0000

    using synchronization markers

--- scale/all_reduce_vgg_model.py
+++ scale/all_reduce_vgg_model.py
@@ -22,6 +22,7 @@ nGPUs = torch.cuda.device_count()
 def time_all_reduce_vgg_model_size(repeat=12, discard=2):
     print('repeat', repeat, 'discard', discard)
     times_per_iteration = []
+
     for _ in range(repeat):
         tensors = [torch.FloatTensor(size).fill_(const_val) for i in range(nGPUs)]  # dim size, value random
         expected = torch.FloatTensor(size).zero_()
@@ -29,20 +30,28 @@ def time_all_reduce_vgg_model_size(repeat=12, discard=2):
             expected.add_(tensor)  # add in-place on CPU
 
         tensors = [tensors[i].cuda(i) for i in range(nGPUs)]  # move ith tensor into ith GPU
-
-        start_time = timer.time()
+        torch.cuda.synchronize()
+        
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
         nccl.all_reduce(tensors)
-        times_per_iteration.append(timer.time() - start_time)
+        torch.cuda.synchronize()
+        end.record()
+
+        times_per_iteration.append(start.elapsed_time(end))  # milli secs
+
         for tensor in tensors:
             assert torch.all(torch.eq(tensor.cpu(), expected))  # move to CPU and compare
+
     times_per_iteration = times_per_iteration[discard:]  # discard first few
     print(len(times_per_iteration), times_per_iteration)
     avg_time_taken = sum(times_per_iteration)/(repeat - discard)
     return avg_time_taken
 
-reduction_time = time_all_reduce_vgg_model_size(22, 2)
+reduction_time = time_all_reduce_vgg_model_size(12, 2)
 
 print('Python VERSION:', sys.version)
 print('pyTorch VERSION:', torch.__version__)
 print ('Available GPUs ', nGPUs)
-print("Time taken:{:.9f} seconds".format(reduction_time))
+print("Time taken:{:.9f} milliseconds".format(reduction_time))
