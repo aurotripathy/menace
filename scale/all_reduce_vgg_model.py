1243e14460770c668fe4dbd7df5b929db7782bbe
Auro Tripathy <ipserv@yahoo.com>
2019-05-19 16:37:18 -0700

    added more comments

--- scale/all_reduce_vgg_model.py
+++ scale/all_reduce_vgg_model.py
@@ -1,5 +1,5 @@
 """
-Simple test to all_reduce_sum a set of model parameters the size of the VGG-16 model (~138M floats)
+Time the all_reduce_sum operation with model parameters size of the VGG-16 model (~138M floats)
     1. Paramters are loaded into each of the N=4 GPUs
     2. nccl.all_reduce is invoked on the paramters
 TO get a breakdown of the VGG model size, see...
@@ -10,10 +10,9 @@ from __future__ import print_function
 import torch
 import torch.cuda.nccl as nccl
 import torch.cuda
-import time as timer
 import sys
 
-model_size = 138000000
+model_size = 138000000  # of VGG-16
 const_val = 5.0
 
 nGPUs = torch.cuda.device_count()
@@ -31,7 +30,6 @@ def time_all_reduce_vgg_model_size(repeat=12, discard=2):
 
         tensors = [tensors[i].cuda(i) for i in range(nGPUs)]  # move ith tensor into ith GPU
         torch.cuda.synchronize()  # wait for move to complete
-        
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
         start.record()
@@ -39,7 +37,7 @@ def time_all_reduce_vgg_model_size(repeat=12, discard=2):
         torch.cuda.synchronize()  # wait for all_reduce to complete
         end.record()
 
-        torch.cuda.synchronize()  # need to wait for op to finish
+        torch.cuda.synchronize()  # need to wait once more for op to finish
         times_per_iteration.append(start.elapsed_time(end))  # millisecs
 
         for tensor in tensors:
@@ -56,5 +54,5 @@ print('Python VERSION:', sys.version)
 print('PyTorch VERSION:', torch.__version__)
 print('RCCL VERSION:', torch.cuda.nccl.version())
 print ('Available GPUs ', nGPUs)
-print("Time taken to all_deduce {} float tensors:{:.6f} milliseconds".format(model_size, reduction_time))
+print("Time to all_reduce {} float tensors:{:.6f} milliseconds".format(model_size, reduction_time))
 print("Ring reduce rate:{:.6} GB/s".format((model_size * 4 * 2 * (nGPUs -1) / nGPUs) / (reduction_time /1000) /1.0e9))
