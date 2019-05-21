61e0fc16ba65408e2071024f9fab73a57039ed2b
Auro Tripathy <ipserv@yahoo.com>
2019-05-21 20:58:52 +0000

    now uses just nn.DataParallel

--- data-parallel/mnist.py
+++ data-parallel/mnist.py
@@ -4,12 +4,10 @@ import logging
 import os
 import sys
 import torch
-import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data
-import torch.utils.data.distributed
 from torchvision import datasets, transforms
 
 logger = logging.getLogger(__name__)
@@ -37,15 +35,14 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-def _get_train_data_loader(batch_size, training_dir, is_distributed, **kwargs):
+def _get_train_data_loader(batch_size, training_dir, **kwargs):
     logger.info("Get train data loader")
     dataset = datasets.MNIST(training_dir, train=True, transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ]), download=True)
-    train_sampler = torch.utils.data.distributed.DistributedSampler(dataset) if is_distributed else None
-    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=train_sampler is None,
-                                       sampler=train_sampler, **kwargs)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True,
+                                       **kwargs)
 
 
 def _get_test_data_loader(test_batch_size, training_dir, **kwargs):
@@ -58,38 +55,18 @@ def _get_test_data_loader(test_batch_size, training_dir, **kwargs):
         batch_size=test_batch_size, shuffle=True, **kwargs)
 
 
-def _average_gradients(model):
-    # Gradient averaging.
-    size = float(dist.get_world_size())
-    for param in model.parameters():
-        dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM, group=0)
-        param.grad.data /= size
-
-
 def train(args):
-    is_distributed = len(args.hosts) > 1 and args.backend is not None
-    logger.debug("Distributed training - {}".format(is_distributed))
     use_cuda = args.num_gpus > 0
     logger.debug("Number of gpus available - {}".format(args.num_gpus))
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     device = torch.device("cuda" if use_cuda else "cpu")
-
-    if is_distributed:
-        # Initialize the distributed environment.
-        world_size = len(args.hosts)
-        os.environ['WORLD_SIZE'] = str(world_size)
-        host_rank = args.hosts.index(args.current_host)
-        dist.init_process_group(backend=args.backend, rank=host_rank, world_size=world_size)
-        logger.info('Initialized the distributed environment: \'{}\' backend on {} nodes. '.format(
-            args.backend, dist.get_world_size()) + 'Current host rank is {}. Number of gpus: {}'.format(
-            dist.get_rank(), args.num_gpus))
 
     # set the seed for generating random numbers
     torch.manual_seed(args.seed)
     if use_cuda:
         torch.cuda.manual_seed(args.seed)
 
-    train_loader = _get_train_data_loader(args.batch_size, args.data_dir, is_distributed, **kwargs)
+    train_loader = _get_train_data_loader(args.batch_size, args.data_dir, **kwargs)
     test_loader = _get_test_data_loader(args.test_batch_size, args.data_dir, **kwargs)
 
     logger.debug("Processes {}/{} ({:.0f}%) of train data".format(
@@ -103,12 +80,8 @@ def train(args):
     ))
 
     model = Net().to(device)
-    if is_distributed and use_cuda:
-        # multi-machine multi-gpu case
-        model = torch.nn.parallel.DistributedDataParallel(model)
-    else:
-        # single-machine multi-gpu case or single-machine or multi-machine cpu case
-        model = torch.nn.DataParallel(model)
+    # single-machine multi-gpu case or single-machine or multi-machine cpu case
+    model = torch.nn.DataParallel(model)
 
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
@@ -120,9 +93,6 @@ def train(args):
             output = model(data)
             loss = F.nll_loss(output, target)
             loss.backward()
-            if is_distributed and not use_cuda:
-                # average gradients manually for multi-machine cpu case only
-                _average_gradients(model)
             optimizer.step()
             if batch_idx % args.log_interval == 0:
                 logger.info('Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
@@ -150,14 +120,6 @@ def test(model, test_loader, device):
         100. * correct / len(test_loader.dataset)))
 
 
-def model_fn(model_dir):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = torch.nn.DataParallel(Net())
-    with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
-        model.load_state_dict(torch.load(f))
-    return model.to(device)
-
-
 def save_model(model, model_dir):
     logger.info("Saving the model.")
     path = os.path.join(model_dir, 'model.pth')
@@ -183,14 +145,10 @@ if __name__ == '__main__':
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--backend', type=str, default=None,
-                        help='backend for distributed training (tcp, gloo on cpu and gloo, nccl on gpu)')
 
-    # Container environment
-    parser.add_argument('--hosts', type=list, default=[None])
-    # parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
+
     parser.add_argument('--model-dir', type=str, default='model')
     parser.add_argument('--data-dir', type=str, default='train-data')
-    parser.add_argument('--num-gpus', type=int, default=3)
+    parser.add_argument('--num-gpus', type=int, default=4)
 
     train(parser.parse_args())
