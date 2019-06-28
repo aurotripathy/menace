e3c7b761be443ea5526fca88946293c1bb6518af
Auro Tripathy <ipserv@yahoo.com>
2019-06-28 23:09:05 +0000

    all flavors included

--- scale/run_ResneXt_largest.sh
+++ scale/run_ResneXt_largest.sh
@@ -1,1 +1,4 @@
-HSA_FORCE_FINE_GRAIN_PCIE=1 python2.7 micro_benchmarking_pytorch.py --dataparallel --device_ids=0,1,2,3 --iterations 10 --batch-size 64
+HSA_FORCE_FINE_GRAIN_PCIE=1 python2.7 micro_benchmarking_pytorch.py --network ResNeXt101_32C_8d  --dataparallel --device_ids=0,1,2,3 --iterations 10 --batch-size 64
+HSA_FORCE_FINE_GRAIN_PCIE=1 python2.7 micro_benchmarking_pytorch.py --network ResNeXt101_32C_16d --dataparallel --device_ids=0,1,2,3 --iterations 10 --batch-size 64
+HSA_FORCE_FINE_GRAIN_PCIE=1 python2.7 micro_benchmarking_pytorch.py --network ResNeXt101_32C_32d --dataparallel --device_ids=0,1,2,3 --iterations 10 --batch-size 64
+HSA_FORCE_FINE_GRAIN_PCIE=1 python2.7 micro_benchmarking_pytorch.py --network ResNeXt101_32C_48d --dataparallel --device_ids=0,1,2,3 --iterations 10 --batch-size 64
