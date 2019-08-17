b03715f1f8ffda12fe6b075857782ce8ee2c24a8
Auro Tripathy <ipserv@yahoo.com>
2019-08-17 16:22:31 -0700

    added SiLU activation

--- all-optim-all-act-mnist/custom_activations.py
+++ all-optim-all-act-mnist/custom_activations.py
@@ -1,10 +1,43 @@
+"""
+Package as a subclass of PyTorch nn.Module, so
+the function now can be easily used in training models
+
+"""
 import torch
 import torch.nn as nn
 import math
 
-class Gelu(nn.Module):
+class GeLU(nn.Module):
     def __init__(self):
         super(Gelu, self).__init__()
 
     def forward(self, x):
         return 0.5 * x * (1 + torch.tanh(math.sqrt(math.pi / 2) * (x + 0.044715 * x ** 3)))
+
+class SiLU(nn.Module):
+    '''
+    Applies the Sigmoid Linear Unit (SiLU) function element-wise:
+        SiLU(x) = x * sigmoid(x)
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+    References:
+        -  Related paper:
+        https://arxiv.org/pdf/1606.08415.pdf
+    Examples:
+        >>> m = silu()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+    '''
+    def __init__(self):
+        '''
+        Init method.
+        '''
+        super().__init__() # init the base class
+
+    def forward(self, x):
+        '''
+        Forward pass of the function.
+        '''
+        return x * torch.sigmoid(x)
