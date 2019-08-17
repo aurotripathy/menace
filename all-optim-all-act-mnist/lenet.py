78ad71a59d60170919ad31a42ba8e9a095a370a8
Auro Tripathy <ipserv@yahoo.com>
2019-08-17 16:22:41 -0700

    added SiLU activation

--- all-optim-all-act-mnist/lenet.py
+++ all-optim-all-act-mnist/lenet.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from collections import OrderedDict
+import custom_activations
 
 
 class LeNet5(nn.Module):
@@ -16,23 +17,27 @@ class LeNet5(nn.Module):
     tanh
     F7 - 10 (Output)
     """
-    def __init__(self, activation='ReLU'):
+    def __init__(self, activation_str='ReLU'):
         super(LeNet5, self).__init__()
-        activation_fn = getattr(nn, activation)
+
+        if activation_str == 'GeLU' or activation_str == 'SiLU':
+            activation_fn = getattr(custom_activations, activation_str)
+        else:
+            activation_fn = getattr(nn, activation_str)
         self.convnet = nn.Sequential(OrderedDict([
             ('c1', nn.Conv2d(1, 6, kernel_size=(5, 5))),
-            ('relu1', activation_fn()),
+            ('activation1', activation_fn()),
             ('s2', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
             ('c3', nn.Conv2d(6, 16, kernel_size=(5, 5))),
-            ('relu3', activation_fn()),
+            ('activation3', activation_fn()),
             ('s4', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
             ('c5', nn.Conv2d(16, 120, kernel_size=(5, 5))),
-            ('relu5', activation_fn())
+            ('activation5', activation_fn())
         ]))
 
         self.fc = nn.Sequential(OrderedDict([
             ('f6', nn.Linear(120, 84)),
-            ('relu6', activation_fn()),
+            ('activation6', activation_fn()),
             ('f7', nn.Linear(84, 10)),
             ('sig7', nn.LogSoftmax(dim=-1))
         ]))
