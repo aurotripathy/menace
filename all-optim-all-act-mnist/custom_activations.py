fa4ef9fc63b1b53ea9f98f55e130c810cc11eae3
Auro Tripathy <ipserv@yahoo.com>
2019-08-17 16:38:44 -0700

    v0.0

--- all-optim-all-act-mnist/custom_activations.py
+++ all-optim-all-act-mnist/custom_activations.py
@@ -9,7 +9,7 @@ import math
 
 class GeLU(nn.Module):
     def __init__(self):
-        super(Gelu, self).__init__()
+        super(GeLU, self).__init__()
 
     def forward(self, x):
         return 0.5 * x * (1 + torch.tanh(math.sqrt(math.pi / 2) * (x + 0.044715 * x ** 3)))
