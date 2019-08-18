61c5dd8e50f3294440b1dcc810013f3006afcb58
Auro Tripathy <ipserv@yahoo.com>
2019-08-18 14:46:01 -0700

    added soft exponential activation

--- all-optim-all-act-mnist/custom_activations.py
+++ all-optim-all-act-mnist/custom_activations.py
@@ -1,12 +1,12 @@
 """
-Additional activation functions not rasdily aavilable in PyTorch
-packaged here as a subclass of PyTorch nn.Module.
+Additional activation functions not available in PyTorch are
+packaged here as a subclass of nn.Module.
 The activation function can now be easily used in training models
-
 """
+
+import math
 import torch
 import torch.nn as nn
-import math
 
 class GeLU(nn.Module):
     def __init__(self):
@@ -28,3 +28,34 @@ class SiLU(nn.Module):
 
     def forward(self, x):
         return x * torch.sigmoid(x)
+
+
+class SoftExponential(nn.Module):
+    '''
+    Parameters:
+        - alpha - trainable parameter
+    References:
+        https://arxiv.org/pdf/1602.01321.pdf
+    '''
+    def __init__(self, alpha=None):
+
+        super(SoftExponential, self).__init__()
+
+        # initialize alpha as trainable tensor
+        if alpha is None:
+            self.alpha = torch.tensor(0.0)
+        else:
+            self.alpha = torch.tensor(alpha)
+
+        self.alpha.requires_grad = True # trainable, set requiresGrad to true
+
+    def forward(self, x):
+
+        if self.alpha == 0.0:
+            return x
+
+        if self.alpha < 0.0:
+            return - torch.log(1 - self.alpha * (x + self.alpha)) / self.alpha
+
+        if self.alpha > 0.0:
+            return (torch.exp(self.alpha * x) - 1)/ self.alpha + self.alpha
