47eb988a22df3404ac3d981dcd63409233d356c6
Auro Tripathy <ipserv@yahoo.com>
2019-10-17 19:29:16 +0000

    for container support

--- rl/rl_a3c_pytorch/run_train.sh
+++ rl/rl_a3c_pytorch/run_train.sh
@@ -1,2 +1,2 @@
-python3.6 main.py --env MsPacman-v0 --workers 220 --gpu-ids 0 1 2 3 4 5 6 7 --amsgrad True
+python3.6 main.py --env MsPacman-v0 --workers 100 --log-dir /data/rl/logs --save-model-dir /data/rl/trained_models --gpu-ids 0 1 2 3 4 5 6 7 --amsgrad True
 
