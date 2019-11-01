e320aec8507c67fe7c2df873d7b8c7f12cf83911
Auro Tripathy <ipserv@yahoo.com>
2019-11-01 06:19:28 +0000

    all text in

--- rl/rl_a3c_pytorch/gym-matplotlib-animated-eval.py
+++ rl/rl_a3c_pytorch/gym-matplotlib-animated-eval.py
@@ -92,10 +92,17 @@ def updatefig(*fargs):
 fig = plt.figure(figsize=(7, 7), facecolor='black')
 fig.canvas.set_window_title('Reinforcement Learning')
 ax = fig.add_subplot(1, 1, 1, aspect=1)
-# ax.set_title("RL Plays Pacman", fontsize=20, verticalalignment='bottom', color='r')
-ax.set_title("RL Plays Pacman", fontsize=30, color='r')
-plt.text(-2, 220, "Trained for 10 hours",
-         color='w', fontsize=18)
+# ax.set_title("PyTorch Plays Pacman", fontsize=20, verticalalignment='bottom', color='r')
+ax.set_title("PyTorch Plays Pacman", fontsize=32, color='r')
+footer_text = 'Model saved at '.format(args.train_time)
+plt.text(-20, 220, footer_text,
+         color='w', fontsize=20)
+checkpoint_time = '{} minute mark'.format(args.train_time)
+plt.text(66, 220, checkpoint_time,
+         color='r', fontsize=20)
+infomercial = 'Trained on 100 EPYC cores and 8 MI50 GPUs'.format(args.train_time)
+plt.text(-30, 230, infomercial,
+         color='w', fontsize=16)
 
 im = plt.imshow(env.render(mode='rgb_array'), animated=True)
         
