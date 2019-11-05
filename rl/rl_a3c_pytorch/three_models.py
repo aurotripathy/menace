a8f5e85f8867294c987f30d31fa941bafa04aa72
Auro Tripathy <ipserv@yahoo.com>
2019-11-05 07:17:53 +0000

    tweak

--- rl/rl_a3c_pytorch/three_models.py
+++ rl/rl_a3c_pytorch/three_models.py
@@ -9,10 +9,10 @@ cmd = "python3.6 gym-matplotlib-animated-eval.py \
 
 print(cmd)
 
-train_times = [10, 100, 1000]
-sleep_times = [15, 20, 60]
-model_locations = ['/dockerx/data/rl/trained_models/',
-                   '/dockerx/data/rl/trained_models/',
+train_times = [50, 150, 530]
+sleep_times = [25, 25, 60]
+model_locations = ['/dockerx/data/rl/trained_models-53m/',
+                   '/dockerx/data/rl/trained_models-150m/',
                    '/dockerx/data/rl/trained_models/',]
 while True:
     for train_time, sleep_time, model_location in zip(train_times,
