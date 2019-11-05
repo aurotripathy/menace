ea7ce01c82be1d55a1b59d01f5440f174d5b755e
Auro Tripathy <ipserv@yahoo.com>
2019-11-05 23:26:35 +0000

    animated

--- rl/rl_a3c_pytorch/monitor/graph_logs.py
+++ rl/rl_a3c_pytorch/monitor/graph_logs.py
@@ -13,23 +13,15 @@ import numpy as np
 import argparse
 import os
 
+import matplotlib.animation as animation
+
+
 SCORE_COL = 4
 TIME_COL = 0
 ROWS_TO_SKIP = 19
 
-def display(time, scores, save_path):
 
-    fig, ax = plt.subplots()
-    ax.plot(time, scores)
-
-    ax.set(xlabel='time (s)', ylabel='scores',
-       title='Scores over time')
-    ax.grid()
-
-    fig.savefig(save_path)
-    plt.show()
-
-def refresh_graph(log_path):
+def load_graph(log_path):
     time_axis = []
     scores_axis = []
     df = pd.read_csv(log_path, header=None,
@@ -46,15 +38,12 @@ def refresh_graph(log_path):
                            start_time).total_seconds())
         scores_axis.append(float(df[SCORE_COL][time].split()[2]))
 
-    print('{}'.format([[t, s] for t, s in zip(time_axis[-10:], scores_axis[-10:])]))
-    save_path = os.path.join(os.path.dirname(log_path), 'scores_plot.png')
-    display(time_axis[0:], scores_axis[0:], save_path)
+    return time_axis[0:], scores_axis[0:]
 
-class MyHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print(f'event type: {event.event_type}  path : {event.src_path}')
-        refresh_graph()
-
+def animate(num, x, y, line):
+    line.set_data(x[:num], y[:num])
+    return line,
+    
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -64,14 +53,17 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    refresh_graph(args.log_path)
-    event_handler = MyHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path='./../logs', recursive=False)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    times, scores = load_graph(args.log_path)
+
+    # First set up the figure, the axis, and the plot element we want to animate
+    fig, ax = plt.subplots()
+    line, = ax.plot(times, scores, color='r')
+
+    ax.set(xlabel='time (s)', ylabel='scores',
+       title='Scores over time')
+    ax.grid()
+
+    ani = animation.FuncAnimation(fig, animate, len(times), fargs=[times, scores, line],
+                                  interval=75, blit=False)    
+    
+    plt.show()
