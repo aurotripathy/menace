dd3dcdd07eb3e8d8f33fad2d91a7b319ccea684d
Auro Tripathy <ipserv@yahoo.com>
2019-11-05 18:52:30 +0000

    test code

---
+++ rl/rl_a3c_pytorch/test/listener.py
@@ -0,0 +1,17 @@
+# https://stackoverflow.com/questions/6920858/interprocess-communication-in-python?rq=1
+
+from multiprocessing.connection import Listener
+
+address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
+listener = Listener(address, authkey=str.encode('sc19-visuals'))
+conn = listener.accept()
+print('connection accepted from', listener.last_accepted)
+while True:
+    msg = conn.recv()
+    # do something with msg
+    if msg == 'close':
+        print("Got message", msg)
+        # conn.close()
+        # break
+listener.close()
+print('Listener done.')
