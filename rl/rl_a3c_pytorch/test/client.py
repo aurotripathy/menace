61ed51200cb95809a953e8fe64aa03fdbe2e2baa
Auro Tripathy <ipserv@yahoo.com>
2019-11-09 15:56:30 -0800

    cosmetic

--- rl/rl_a3c_pytorch/test/client.py
+++ rl/rl_a3c_pytorch/test/client.py
@@ -6,7 +6,7 @@ import time
 address = ('localhost', 6000)
 conn = Client(address, authkey=str.encode('sc19-visuals'))
 while True:
-    conn.send('close')
+    conn.send('next')
     time.sleep(3)
     # can also send arbitrary objects:
     # conn.send(['a', 2.5, None, int, sum])
