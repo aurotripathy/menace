27327c732c7480219f008f099b47e722d2ee3ade
Auro Tripathy <ipserv@yahoo.com>
2019-11-10 16:24:26 +0000

    dummy client

---
+++ rl/rl_a3c_pytorch/monitor/client.py
@@ -0,0 +1,17 @@
+#Your client could send commands as objects:
+
+from multiprocessing.connection import Client
+import time
+
+address = ('localhost', 6000)
+conn = Client(address, authkey=str.encode('sc19-visuals'))
+print('got a connection')
+while True:
+    print('sending message')
+    conn.send('next')
+    
+    time.sleep(3)
+    # can also send arbitrary objects:
+    # conn.send(['a', 2.5, None, int, sum])
+# conn.close()
+print('Client done')
