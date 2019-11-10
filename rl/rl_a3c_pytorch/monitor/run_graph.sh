5d8dad6c02e4bbcb7913a71eecec61708b999909
Auro Tripathy <ipserv@yahoo.com>
2019-11-10 16:40:06 +0000

    wip

--- rl/rl_a3c_pytorch/monitor/run_graph.sh
+++ rl/rl_a3c_pytorch/monitor/run_graph.sh
@@ -1,1 +1,2 @@
-python graph_logs.py --log-path /dockerx/data/rl/logs/MsPacman-v0_log 
+# python graph_logs.py --log-path /dockerx/data/rl/logs/MsPacman-v0_log
+python graph_logs.py --log-path /dockerx/data/rl/logs-690/MsPacman-v0_log
