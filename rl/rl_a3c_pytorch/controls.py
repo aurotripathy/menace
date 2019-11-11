fcf30ae58122d4ee8cefb4b24e56b1380deebdb1
Auro Tripathy <ipserv@yahoo.com>
2019-11-11 19:24:46 +0000

    batter names

--- rl/rl_a3c_pytorch/controls.py
+++ rl/rl_a3c_pytorch/controls.py
@@ -6,32 +6,34 @@ from three_models import process_in_sequence
 root = tk.Tk()
 root.title('CONTROLS')
 
-helv36 = font.Font(family="Helvetica", size=20, weight="bold")
+# helv20 = font.Font(family="Helvetica", size=20, weight="bold")
+helv20 = font.Font(family="Helvetica", size=20)
 
 v = tk.IntVar()
-v.set(1)  # initializing the choice, i.e. Python
+v.set(3)  # initializing the choice, i.e. Python
 
-languages = [
+options = [
     ("Play w/Model 1"),
     ("Play w/Model 2"),
     ("Play w/Model 3"),
-    ("AutoPlay All"),
+    ("AutoPlay All Models"),
 ]
 
 def process_choice():
     print('You selected:', v.get())
-    process_in_sequence()
+    if v.get() == 3:
+        process_in_sequence()
     
-tk.Label(root, font=helv36,
+tk.Label(root, font=helv20,
          text="""Choose Model:""",
          justify = tk.LEFT,
          padx = 20).pack()
 
-for val, language in enumerate(languages):
+for val, option in enumerate(options):
     tk.Radiobutton(root, 
-            text=language,
-            font=helv36,
-            indicatoron = 0,  # indicator on
+            text=option,
+            font=helv20,
+                   indicatoron = 0,  # indicator on, full txt in box
             width = 20,
             padx = 20, 
             variable=v, 
