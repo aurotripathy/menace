d6c094f2bf177f2579cbc0bb339cc193deca9f6e
Auro Tripathy <ipserv@yahoo.com>
2019-11-12 01:40:37 +0000

    consolidating strings

--- rl/rl_a3c_pytorch/credits.py
+++ rl/rl_a3c_pytorch/credits.py
@@ -1,5 +1,5 @@
 
 infomercial_str = 'Trained on 100 AMD EPYC$^{TM}$ cores and \n\t\t8 AMD Radeon Instinct$^{TM}$ MI50 GPUs'
 
-credits_str = 'PAC-MAN Game Acknowledgements | Source: \n MIT: OpenAI Gym Authors: Greg Brockman, Vicki Cheung, Ludwig Pettersson, Jonas Schneider, John Schulman and Jie Tang https://github.com/openai/gym\n  MIT: gym Author: Vikash Kumar https://github.com/openai/gym/blob/master/LICENSE.md \n Apache 2.0: A3C LSTM Atari with Pytorch plus A3G design Author: David Griffis https://github.com/dgriff777/rl_a3c_pytorch\n'
+credits_str = 'PAC-MAN Game Acknowledgements | Source: \nMIT: OpenAI Gym Authors: Greg Brockman, Vicki Cheung, Ludwig Pettersson, Jonas Schneider, John Schulman and Jie Tang https://github.com/openai/gym\nMIT: gym Author: Vikash Kumar https://github.com/openai/gym/blob/master/LICENSE.md \nApache 2.0: A3C LSTM Atari with Pytorch plus A3G design Author: David Griffis https://github.com/dgriff777/rl_a3c_pytorch\n'
 
